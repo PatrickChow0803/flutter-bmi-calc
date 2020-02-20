006ec924c52de6d138e32cab533a2230e5b1dcff
patrick <patrickchow0803@gmail.com>
2020-02-20 17:05:41 -0500

    Added custom widget and refactored some code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reuseable_card.dart';
 
 /*
 
@@ -10,7 +13,7 @@ import 'package:flutter/material.dart';
 
  */
 
-const BOTTOM_CONTAINER_HEIGHT = 80.0;
+const BOTTOM_CONTAINER_HEIGHT = 50.0;
 const ACTIVE_CARD_COLOR = Color(0xFF1D1E33);
 const BOTTOM_CONTAINER_COLOR = Color(0xFFEB1555);
 
@@ -34,25 +37,43 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReuseableCard(color: ACTIVE_CARD_COLOR),
+                  child: ReuseableCard(
+                    color: ACTIVE_CARD_COLOR,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
+                  ),
                 ),
                 Expanded(
-                  child: ReuseableCard(color: ACTIVE_CARD_COLOR),
+                  child: ReuseableCard(
+                    color: ACTIVE_CARD_COLOR,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReuseableCard(color: ACTIVE_CARD_COLOR),
+            child: ReuseableCard(
+              color: ACTIVE_CARD_COLOR,
+            ),
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReuseableCard(color: ACTIVE_CARD_COLOR),
+                  child: ReuseableCard(
+                    color: ACTIVE_CARD_COLOR,
+                  ),
                 ),
                 Expanded(
-                  child: ReuseableCard(color: ACTIVE_CARD_COLOR),
+                  child: ReuseableCard(
+                    color: ACTIVE_CARD_COLOR,
+                  ),
                 ),
               ],
             ),
@@ -64,25 +85,6 @@ class _InputPageState extends State<InputPage> {
             height: BOTTOM_CONTAINER_HEIGHT,
           ),
         ],
-      ),
-    );
-  }
-}
-
-// Card that displays information on the screen
-class ReuseableCard extends StatelessWidget {
-  ReuseableCard({@required this.color});
-
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15),
-//        Use decoration to give boxes a rounded edge
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
---
+++ lib/reuseable_card.dart
@@ -0,0 +1,22 @@
+// Card that displays information on the screen
+import 'package:flutter/material.dart';
+
+class ReuseableCard extends StatelessWidget {
+  ReuseableCard({@required this.color, this.cardChild});
+
+  final Color color;
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15),
+//        Use decoration to give boxes a rounded edge
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
