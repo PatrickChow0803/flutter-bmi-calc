902a58ff4faab71d8cb69b9e05d316ef511d820d
patrick <patrickchow0803@gmail.com>
2020-02-19 20:18:36 -0500

    Added a custom theme and moved the StatefulWidget code into a different file

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'input_page.dart';
+
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // copyWith makes it so that I can use preset themes but just update specific things that I want to change.
+      theme: ThemeData.dark().copyWith(
+//      0xFF is needed when working with Hex colors
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+//          For changing the text color
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.white),
+        ),
+      ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
