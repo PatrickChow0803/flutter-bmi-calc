bf81e595800f2e027cd56a2cacffc81f1d17b76b
patrick <patrickchow0803@gmail.com>
2020-02-19 20:47:11 -0500

    Added a reuseablecard class to represent the multiple cards on the screen.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,57 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      // A column has 3 areas horizontally in my app. Therefore add 3 expanded widgets to the column.
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            // A row has 2 areas vertically in my app. Therefore add 2 expanded widgets to the row.
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReuseableCard(color: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReuseableCard(color: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReuseableCard(color: Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReuseableCard(color: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReuseableCard(color: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// Card that displays information on the screen
+class ReuseableCard extends StatelessWidget {
+  ReuseableCard({@required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+//        Use decoration to give boxes a rounded edge
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
